9128d127b5142e1a5ae11f7cc2b4a766b1663f4e
DaviFreiredaSilva <dfsfreire@gmail.com>
2022-01-14 09:13:50 -0300

    React router ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-
+import {Routes, Route, Link} from 'react-router-dom'
 import NavbarComponent from "./components/NavbarComponent/NavbarComponent"
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import BeerPage from './pages/BeerPage';
+import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent/>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<HomePage/>}/>
+          <Route path="/products" element={<ProductsPage/>}/>
+          <Route path="/beer" element={<BeerPage/>}/>
+          <Route path="/contact" element={<ContactPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -1,21 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import {Link} from 'react-router-dom'
 import {FaUser, FaPhoneAlt} from "react-icons/fa"
 
 const NavbarComponent = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">LOJA DE BEBIDAS</Navbar.Brand>
+        <Navbar.Brand href="/">LOJA DE BEBIDAS</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink href="#home">Início</NavLink>
-            <NavLink href="#link">Todos os produtos</NavLink>
-            <NavLink href="#link">Cerveja</NavLink>
-            <NavLink href="#link"><FaPhoneAlt/> Contato</NavLink>
-            <NavLink href="#link"><FaUser/> Login</NavLink>
+            <NavLink to="/">Início</NavLink>
+            <NavLink to="/products">Todos os produtos</NavLink>
+            <NavLink to="/beer">Cerveja</NavLink>
+            <NavLink to="/contact"><FaPhoneAlt/> Contato</NavLink>
+            <NavLink to="/login"><FaUser/> Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -23,10 +24,11 @@ const NavbarComponent = () => {
   );
 };
 
-const NavLink = styled(Nav.Link)`
+const NavLink = styled(Link)`
   color: white !important;
   margin-right: 5vw;
   transition: 0.3s;
+  text-decoration: none;
   &:hover {
     color: #dd9177 !important;
   }
